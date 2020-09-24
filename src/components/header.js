fac49d0a11cd17ac89d84a77608ff36ade820e5c
Christian Sarnataro <christian.sarnataro@vimnmix.com>
2020-09-24 19:39:58 +0200

    feat(style): added tailwind css support
    Added tailwind css support, postcss, and a test component with some styling

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+  return <div className="text-center text-yellow-400">
+      <div className="text-5xl font-bold">SWAPI</div>
+      <div className="text-lg">The Star Wars API</div>
+    </div>;
+}
+
+export default Header;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
-import Layout from '../components/layouts';
-import Header from '../components/header';
 
-function HomePage() {
-  return <Layout>
+function AboutPage() {
+  return <div>
     <Head>
       <title>SWAPI - The Star Wars API</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     </Head>
-    <Header />
-  </Layout>;
+    What is this?
+  </div>;
 }
 
-export default HomePage;
+export default AboutPage;
