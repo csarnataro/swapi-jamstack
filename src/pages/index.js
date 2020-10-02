0a66a425c67c29fd7c184057b0c4fd539325d11b
Christian Sarnataro <christian.sarnataro@vimnmix.com>
2020-10-02 12:18:05 +0200

    feat(index): added dynamic content
    Added dynamically content fetching the json responses from API.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
+// import fetch from 'node-fetch';
 import Layout from '../components/layout';
 import RequestBox from '../components/request-box';
 
-function HomePage() {
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const serverName = process.env.NEXT_PUBLIC_API_SERVER_NAME;
+  const res = await global.fetch(`${serverName}/api/people/1`);
+  const initialCode = await res.json();
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      initialCode: JSON.stringify(initialCode, null, 2),
+    },
+  };
+}
+
+function HomePage({ initialCode }) {
   return (
     <Layout currentPage="index">
       <Head>
@@ -14,35 +32,38 @@ function HomePage() {
         ></meta>
       </Head>
       <div className="container mx-auto text-center text-md pt-4 pb-12">
-        <div>All the Star Wars data you’ve ever wanted:
-          Planets, Spaceships, Vehicles, People, Films and Species</div>
-        <div>From all SEVEN Star Wars films.
-          Now with The Force Awakens data!</div>
+        <div>
+          All the Star Wars data you’ve ever wanted: Planets, Spaceships,
+          Vehicles, People, Films and Species
+        </div>
+        <div>
+          From all SEVEN Star Wars films. Now with The Force Awakens data!
+        </div>
       </div>
       <div>
-        <RequestBox />
+        <RequestBox initialCode={initialCode} />
 
         <div className="container mx-auto text-center text-md pt-4 pb-12">
           <div className="grid grid-cols-3 gap-4">
             <div>
               <h4 className="text-lg font-bold mb-2">What is this?</h4>
               <p>
-              The Star Wars API, or &ldquo;swapi&rdquo; (Swah-pee) is the
-              world’s first quantified and programmatically-accessible data
-              source for all the data from the Star Wars canon universe! We’ve
-              taken all the rich contextual stuff from the universe and
-              formatted into something easier to consume with software. Then we
-              went and stuck an API on the front so you can access it all!
+                The Star Wars API, or &ldquo;swapi&rdquo; (Swah-pee) is the
+                world’s first quantified and programmatically-accessible data
+                source for all the data from the Star Wars canon universe! We’ve
+                taken all the rich contextual stuff from the universe and
+                formatted into something easier to consume with software. Then
+                we went and stuck an API on the front so you can access it all!
               </p>
             </div>
             <div>
               <h4 className="text-lg font-bold mb-2">How can I use it?</h4>
               <p>
-              All the data is accessible through our HTTP web API. Consult our
-              documentation if you’d like to get started. Helper libraries for
-              popular programming languages are also provided so you can consume
-              swapi in your favourite programming language, in a style that
-              suits you.
+                All the data is accessible through our HTTP web API. Consult our
+                documentation if you’d like to get started. Helper libraries for
+                popular programming languages are also provided so you can
+                consume swapi in your favourite programming language, in a style
+                that suits you.
               </p>
             </div>
 
@@ -51,10 +72,10 @@ function HomePage() {
                 What happened with old swapi.co?
               </h4>
               <p>
-              swapi.co is not supported and maintained anymore. But since so
-              many projects and tutorials used it as their educational
-              playground, this is an &ldquo;unofficial&rdquo; branch This
-              project is open source and you can contribute on GitHub.
+                swapi.co is not supported and maintained anymore. But since so
+                many projects and tutorials used it as their educational
+                playground, this is an &ldquo;unofficial&rdquo; branch This
+                project is open source and you can contribute on GitHub.
               </p>
             </div>
           </div>
@@ -63,5 +84,9 @@ function HomePage() {
     </Layout>
   );
 }
+
+HomePage.propTypes = {
+  initialCode: PropTypes.string,
+};
 
 export default HomePage;
