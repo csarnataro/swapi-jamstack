a9c28d76dfc5440dd42f0d4a73722d6d1be09b1d
Christian Sarnataro <christian.sarnataro@vimnmix.com>
2020-10-13 10:55:36 +0200

    feat(api): Added wookiee encoding
    Added a middleware to intercept a query string parameter called 'format' and to rewrite the res.send function.
    Added a function to translate from plain english to wookiee.

---
+++ functions/lib/wookiee-middleware.js
@@ -0,0 +1,16 @@
+const translateToWookiee = require('./wookiee-formatter');
+
+function wookieMiddleware(req, res, next) {
+  if (req.query.format === 'wookiee') {
+    const originalSend = res.send;
+
+    res.send = (...args) => {
+      const newArgs = args;
+      newArgs[0] = translateToWookiee(args[0]);
+      originalSend.apply(res, newArgs);
+    };
+  }
+  next();
+}
+
+module.exports = wookieMiddleware;
