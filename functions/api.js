be0bb87b67e3a3c0a2e6c3984618ebe6adf3fad8
Christian Sarnataro <csarnataro@users.noreply.github.com>
2021-04-05 16:44:45 +0200

    refactor(app.js): removed app as a serverless function endpoint (#25)
    Renamed app/index.js to app/app.js so that it doesn't appear in the least of available serverless
    functions in Netlify console.

--- functions/api.js
+++ functions/api.js
@@ -3,7 +3,7 @@
  * @see https://www.fastify.io/docs/latest/Serverless/#lambdajs
  */
 const awsLambdaFastify = require('aws-lambda-fastify');
-const init = require('./app');
+const init = require('./app/app');
 
 const proxy = awsLambdaFastify(init());
 exports.handler = proxy;
